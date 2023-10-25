9200e92d059ccbacd94763e2f54cbb0d8ebb3d30
Rafael <rafaelramos0401@example.com>
2023-10-25 13:45:42 -0300

    add: transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,37 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="outcome">- R$ 60,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   );
 }
