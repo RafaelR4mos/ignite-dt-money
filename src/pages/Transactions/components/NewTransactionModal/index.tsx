cfe0c3295a5cc9f3b484f4cb9de737648c605cc4
Rafael <rafaelramos0401@example.com>
2023-10-26 17:15:44 -0300

    add: modal radio btn group with radix

--- src/pages/Transactions/components/NewTransactionModal/index.tsx
+++ src/pages/Transactions/components/NewTransactionModal/index.tsx
@@ -1,6 +1,13 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +24,18 @@ export function NewTransactionModal() {
           <input type="text" required placeholder="Descrição" />
           <input type="number" required placeholder="Preço" />
           <input type="text" required placeholder="Categoria" />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
